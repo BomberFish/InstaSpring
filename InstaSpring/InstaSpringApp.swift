0e0bec1daa065f027363d7c0d1e42f27a40957a1
BomberFish <87151697+BomberFish@users.noreply.github.com>
2022-12-07 16:39:52 -0500

    cleanup

--- InstaSpring/InstaSpringApp.swift
+++ InstaSpring/InstaSpringApp.swift
@@ -15,23 +15,16 @@ struct InstaSpringApp: App {
             ContentView(triggerRespring: $triggerRespring)
                 .scaleEffect(triggerRespring ? 0.95 : 1)
                 .brightness(triggerRespring ? -1 : 0)
-                #if os(iOS) || os(tvOS) || os(watchOS)
                     .statusBarHidden(triggerRespring)
-                #endif
                 .onChange(of: triggerRespring) { _ in
                     if triggerRespring == true {
-                    #if os(iOS)
-                        
                         Timer.scheduledTimer(withTimeInterval: 0.5, repeats: false) { timer in
-                            
-                            // TY amy for respring bug
                             guard let window = UIApplication.shared.windows.first else { return }
                             while true {
                                window.snapshotView(afterScreenUpdates: false)
                             }
                             
                         }
-                    #endif
                     }
                 }
         }
