2fdbcc130ada436eba374ef7beab54570fba6d37
BomberFish <87151697+BomberFish@users.noreply.github.com>
2022-12-07 16:36:46 -0500

    initial commit

--- InstaSpring/ContentView.swift
+++ InstaSpring/ContentView.swift
@@ -8,19 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Binding var triggerRespring: Bool
+    let bg = Color(red: 0, green: 0, blue: 0)
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            
         }
-        .padding()
+        .onAppear{
+            respring()
+
+        }
+        .drawingGroup(opaque: false, colorMode: .linear)
+                .background(
+                    Rectangle()
+                        .foregroundColor(bg))
+                .ignoresSafeArea()
+        }
+    func respring() {
+        withAnimation(.easeInOut) {
+            print("respringing right about now")
+            triggerRespring = true
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(triggerRespring: .constant(false))
     }
 }
--- InstaSpring/InstaSpringApp.swift
+++ InstaSpring/InstaSpringApp.swift
@@ -9,9 +9,31 @@ import SwiftUI
 
 @main
 struct InstaSpringApp: App {
+    @State var triggerRespring = false
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(triggerRespring: $triggerRespring)
+                .scaleEffect(triggerRespring ? 0.95 : 1)
+                .brightness(triggerRespring ? -1 : 0)
+                #if os(iOS) || os(tvOS) || os(watchOS)
+                    .statusBarHidden(triggerRespring)
+                #endif
+                .onChange(of: triggerRespring) { _ in
+                    if triggerRespring == true {
+                    #if os(iOS)
+                        
+                        Timer.scheduledTimer(withTimeInterval: 0.5, repeats: false) { timer in
+                            
+                            // TY amy for respring bug
+                            guard let window = UIApplication.shared.windows.first else { return }
+                            while true {
+                               window.snapshotView(afterScreenUpdates: false)
+                            }
+                            
+                        }
+                    #endif
+                    }
+                }
         }
     }
 }
