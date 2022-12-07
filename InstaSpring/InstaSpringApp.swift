ae0680ea8615a85748b40be2bbbb00806b6b4ca5
BomberFish <87151697+BomberFish@users.noreply.github.com>
2022-12-07 16:02:49 -0500

    Initial Commit

---
+++ InstaSpring/InstaSpringApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaSpringApp.swift
+//  InstaSpring
+//
+//  Created by Hariz Shirazi on 2022-12-07.
+//
+
+import SwiftUI
+
+@main
+struct InstaSpringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
